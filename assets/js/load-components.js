640dab1e33902694cbbe8a25b84948083b538501
nvtoancitctu <nvtoan.1706@gmail.com>
2026-05-08 20:31:15 +0700

    Add reservations and story pages with responsive design and custom styling

---
+++ assets/js/load-components.js
@@ -0,0 +1,57 @@
+async function loadComponent(path, targetId) {
+    const res = await fetch(path);
+    const html = await res.text();
+
+    document.getElementById(targetId).innerHTML = html;
+
+    initScrollButtons();
+}
+
+function initScrollButtons() {
+    const scrollButtons = document.getElementById("scroll-buttons");
+    const topBtn = document.getElementById("scrollTopBtn");
+    const bottomBtn = document.getElementById("scrollBottomBtn");
+
+    // Ẩn ban đầu
+    scrollButtons.classList.add("opacity-0", "pointer-events-none", "translate-y-5");
+
+    // Hiện khi scroll
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            scrollButtons.classList.remove("opacity-0", "pointer-events-none", "translate-y-5");
+
+            scrollButtons.classList.add("opacity-100", "translate-y-0");
+        } else {
+            scrollButtons.classList.add("opacity-0", "pointer-events-none", "translate-y-5");
+
+            scrollButtons.classList.remove("opacity-100", "translate-y-0");
+        }
+    });
+
+    // Lên đầu
+    topBtn?.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+
+    // Xuống cuối
+    bottomBtn?.addEventListener("click", () => {
+        window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: "smooth",
+        });
+    });
+}
+
+// Reload trang khi submit form thành công
+window.addEventListener("pageshow", function (event) {
+    // Nếu trang được load từ cache (back/forward)
+    if (event.persisted || performance.getEntriesByType("navigation")[0].type === "back_forward") {
+        window.location.reload();
+    }
+});
+
+loadComponent("/assets/components/scroll-buttons.html", "scroll-buttons-container");
+loadComponent("/assets/components/footer.html", "footer-container");
